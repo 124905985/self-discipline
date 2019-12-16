4aed6fd55a4d7a13c14deb7a5ab4311a00a8c3fe
suvue <yj020827@163.com>
2019-12-16 22:42:45 +0800

    项目初始化

---
+++ self-discipline/src/main/java/cn/suvue/discipline/SelfDisciplineApplication.java
@@ -0,0 +1,13 @@
+package cn.suvue.discipline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfDisciplineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelfDisciplineApplication.class, args);
+    }
+
+}
---
+++ self-discipline/src/test/java/cn/suvue/discipline/SelfDisciplineApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.suvue.discipline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfDisciplineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
